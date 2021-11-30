ea8fae7b81ac10ac3d03041d6eccce4b5de8497c
Bruno Bezerra <bmb_bezerra@outlook.com>
2021-11-30 11:45:26 -0300

    Estudo Logica

--- logica/bin/main.dart
+++ logica/bin/main.dart
@@ -1,25 +1,9 @@
+import 'package:logica/funcoes.dart';
 import 'package:logica/lacos.dart';
-import 'package:logica/objetos.dart';
 
 void main(List<String> arguments) {
-  //bhaskara(9, -24, 16);
-
-  var animal = Lobo(name: 'Lobinho');
-  animal.setType();
-
-  var person = PersonagemImpl();
-  person.create('Bruno', 'Humano', 'Ladino');
-  person.statics();
+  bhaskara(9, -24, 16);
 
   var timer = Timer();
   timer.start3(5);
-
-  print(animal.name);
-  print(animal.animal);
-  print(animal.type);
-
-  print(person.name);
-  print(person.breed);
-  print(person.atk);
-  print(person.life);
 }
