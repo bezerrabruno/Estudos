0bda8fa3c0adac2bdf2fdbec9d34ed0ddc4a5dc4
Bruno Bezerra <bmb_bezerra@outlook.com>
2021-11-30 11:39:42 -0300

    Estudo Logica

---
+++ logica/lib/condicionais.dart
@@ -0,0 +1,14 @@
+class Test {
+  var x1 = 2;
+  var x2 = 6;
+
+  test() {
+    if (x1 == x2) {
+      print('x1 and x2');
+    } else if (x1 > x2) {
+      print('x1');
+    } else {
+      print('x2');
+    }
+  }
+}
